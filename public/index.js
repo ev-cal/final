4a25e7d3d3c2ee70e90b0b53e748b733f2efbf9c
anshidnm <anshidnm@gmail.com>
2022-01-07 16:41:03 -0800

    next button working

--- public/index.js
+++ public/index.js
@@ -16,6 +16,7 @@ document.getElementById("year").value=year
 
 
 var month=currentDate.getMonth();
+var k=month
 if (month==0) {
     document.getElementById("month").value="January"
 }
@@ -55,6 +56,52 @@ else if (month==11) {
 else{
     document.getElementById("month").value="Loading"
 }
+
+function next_month(){
+    if(window.k>=0 && window.k<=11){
+    window.k=window.k+1;
+    if (window.k==1) {
+        document.getElementById("month").value="February"
+    }
+    else if (window.k==2) {
+        document.getElementById("month").value="March"
+    }
+    else if (window.k==3) {
+        document.getElementById("month").value="April"
+    }
+    else if (window.k==4) {
+        document.getElementById("month").value="May"
+    }
+    else if (window.k==5) {
+        document.getElementById("month").value="June"
+    }
+    else if (window.k==6) {
+        document.getElementById("month").value="July"
+    }
+    else if (window.k==7) {
+        document.getElementById("month").value="August"
+    }
+    else if (window.k==8) {
+        document.getElementById("month").value="September"
+    }
+    else if (window.k==9) {
+        document.getElementById("month").value="October"
+    }
+    else if (window.k==10) {
+        document.getElementById("month").value="November"
+    }
+    else if (window.k==11) {
+        document.getElementById("month").value="December"
+    }}
+
+    
+    
+}
+
+
+
+
+
 
 
 
