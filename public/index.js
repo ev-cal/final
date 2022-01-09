d61973ea4d5d3200b01b2b367df8d2c785e3933c
anshidnm <anshidnm@gmail.com>
2022-01-09 13:08:09 -0800

    Daily view started

--- public/index.js
+++ public/index.js
@@ -19,91 +19,57 @@ document.getElementById("cpyear").value= cp_year
 document.getElementById("year").value = year
 
 
+
 var month = currentDate.getMonth();
-// var k = month
 if (month == 0) {
     document.getElementById("month").value = "January"
+    document.getElementById("dv-month").value = "January"
 }
 else if (month == 1) {
     document.getElementById("month").value = "February"
+    document.getElementById("dv-month").value = "February"
 }
 else if (month == 2) {
     document.getElementById("month").value = "March"
+    document.getElementById("dv-month").value = "March"
 }
 else if (month == 3) {
     document.getElementById("month").value = "April"
+    document.getElementById("dv-month").value = "April"
 }
 else if (month == 4) {
     document.getElementById("month").value = "May"
+    document.getElementById("dv-month").value = "May"
 }
 else if (month == 5) {
     document.getElementById("month").value = "June"
+    document.getElementById("dv-month").value = "June"
 }
 else if (month == 6) {
     document.getElementById("month").value = "July"
+    document.getElementById("dv-month").value = "July"
 }
 else if (month == 7) {
     document.getElementById("month").value = "August"
+    document.getElementById("dv-month").value = "August"
 }
 else if (month == 8) {
     document.getElementById("month").value = "September"
+    document.getElementById("dv-month").value = "September"
 }
 else if (month == 9) {
     document.getElementById("month").value = "October"
+    document.getElementById("dv-month").value = "October"
 }
 else if (month == 10) {
     document.getElementById("month").value = "November"
+    document.getElementById("dv-month").value = "November"
 }
 else if (month == 11) {
     document.getElementById("month").value = "December"
-}
-else {
-    document.getElementById("month").value = "Loading"
+    document.getElementById("dv-month").value = "December"
 }
 
-// function next_month() {
-//     if (window.k >= 0 && window.k <= 13) {
-//         window.k = window.k + 1;
-//         if (window.k == 1) {
-//             document.getElementById("month").value = "February"
-//         }
-//         else if (window.k == 2) {
-//             document.getElementById("month").value = "March"
-//         }
-//         else if (window.k == 3) {
-//             document.getElementById("month").value = "April"
-//         }
-//         else if (window.k == 4) {
-//             document.getElementById("month").value = "May"
-//         }
-//         else if (window.k == 5) {
-//             document.getElementById("month").value = "June"
-//         }
-//         else if (window.k == 6) {
-//             document.getElementById("month").value = "July"
-//         }
-//         else if (window.k == 7) {
-//             document.getElementById("month").value = "August"
-//         }
-//         else if (window.k == 8) {
-//             document.getElementById("month").value = "September"
-//         }
-//         else if (window.k == 9) {
-//             document.getElementById("month").value = "October"
-//         }
-//         else if (window.k == 10) {
-//             document.getElementById("month").value = "November"
-//         }
-//         else if (window.k == 11) {
-//             document.getElementById("month").value = "December"
-//         } else if (window.k == 12) {
-//             document.getElementById("month").value = "January"
-//         } else {
-//             window.k = 0;
-//             next_month();
-//         }
-//     }
-// }
 
 function next_month(){
     const lastday=document.querySelector("#row_31")
